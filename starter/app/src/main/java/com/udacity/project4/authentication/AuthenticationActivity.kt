ebc2f0465b809fc651acfd6c3bfaf185f084d949
Narendra Darla <naren.darla@gmail.com>
2021-02-07 17:36:52 -0800

    Progress

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.udacity.project4.R
 
+
+
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
  * signed in users to the RemindersActivity.
